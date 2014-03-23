a213471c37a6151267b05a212d53a8a73429de66
jgbs <joaogabrieldebrittoesilva@gmail.com>
2014-03-23 10:30:55 -0300

    adicionando css e mudando login

--- Visao/entrarLogin.php
+++ Visao/entrarLogin.php
@@ -1,54 +1,33 @@
-<?php 
+<?php
 session_start();
 $id_usuario = $_SESSION['id_usuario'];
 ?>
 <html>
-<head>	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="http://localhost/SeboEletronicov2.0/Visao/css/UsuarioStyle.css" type="text/css" media="all">
-        <link rel="stylesheet" href="http://localhost/SeboEletronicov2.0/Visao/css/main.css" type="text/css" media="all">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
+        <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
+        <link rel="stylesheet" href="Css/estilo.css"/>
         <link rel="shortcut icon" href="http://localhost/SeboEletronicov2.0/Visao/img/android.ico">
         <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
         <title>Sebo Eletrônico</title>
-        
-</head>
-<body>
-    <div id="header">
-		<div id="logo"><img src="http://localhost/SeboEletronicov2.0/Visao/img/sebo_header.png" class="imgHeader"/></div>
-    </div>
-   
-   <div id="mainmenu">
-       
-       <button class="button" onclick="sair();">Home</button>
-       
-   </div> 
-    
-    <br/>
-    <br/>
-    <br/>
 
-        <form name="loginform" method="post" action="http://localhost/SeboEletronicov2.0/Dao/autenticacaoUsuario.php">
-            <table class='insr'>
-
-                <tr>
-                    <th class='titlein' > <h5>Login do Usuário</h5></th>
-                </tr>
-                
-                <tr> 
-                    <td>
-                        <h4> E-mail: <input type="text" name="email"/></h4> 
-                    </td>
-                </tr>
-                <tr> 
-                    <td>
-                        <h4> Senha: <input type="password" name="senha"/></h4> 
-                    </td>
-                </tr>
-                <th>
-                    <input type="submit" value="Entrar" /><br><br>
-                    <a href="cadastrarUsuario.php">Cadastre-se</a>
-                </th>
-            </table>
-        </form>
-    
+    </head>
+    <body>
+        <div class="container login" style="padding: 30px;">
+            <div class="col-md-4 col-md-offset-4 well-lg login">
+                <div class="formLogin">
+                    <img src="http://localhost/SeboEletronicov2.0/Visao/img/sebo_header.png" style="margin-right: auto; margin-left: auto;" alt="TESTE"/>
+                    <form name="FrmLogin" method="post" action="http://localhost/SeboEletronicov2.0/Dao/autenticacaoUsuario.php" id="FrmLogin" class="form-horizontal" role="form">
+                        <input id="inputCPF" name="cpf" type="text" class="form-control" placeholder="E-mail" required/>
+                        <input id="inputSenha" name="senha" type="password" class="form-control" placeholder="Senha"  maxlength="6" required/>
+                        <input  id="btnEntrar" class="btn btn-lg btn-primary btn-block" type="submit">
+                        <p class="text-center">
+                            <a  href="cadastrarUsuario.php" class="Links" >Cadastre-se</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+    header("location: Visao/entrarLogin.php");
