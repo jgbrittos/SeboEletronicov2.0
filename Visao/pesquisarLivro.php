440661aeb9e1d5361a3fb4ecc5f6eb350e5daf66
jgbs <joaogabrieldebrittoesilva@gmail.com>
2014-03-23 19:08:51 -0300

    alterando a pesquisa de livros - ainda esta com erro pois nao passa os parametros

--- Visao/pesquisarLivro.php
+++ Visao/pesquisarLivro.php
@@ -1,89 +1,56 @@
 <html>
-<head>	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="http://localhost/SeboEletronicov2.0/Visao/css/UsuarioStyle.css" type="text/css" media="all">
-        <link rel="stylesheet" href="http://localhost/SeboEletronicov2.0/Visao/css/main.css" type="text/css" media="all">
-        <link rel="shortcut icon" href="http://localhost/SeboEletronicov2.0/Visao/img/android.ico">
+    <head>	
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
+        <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
+        <link rel="stylesheet" href="Css/estilo.css"/>
         <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
-    <title>Sebo Eletrônico</title>
-    
-</head>
-<body>
-    <div id="header">
-		<div id="logo"><img src="http://localhost/SeboEletronicov2.0/Visao/img/sebo_header.png" class="imgHeader"/></div>
-    </div>
-   
-   <div id="mainmenu">
-       <button class="button" onclick="home()">Home</button>
-       <button class="button" onclick="user();">Usuário</button>       
-       <button class="button" onclick="livro();">Livro</button>
-       <button class="button" onclick="sair();">Sair</button>
-       
-   </div>
-   <div id="mainmenu">
-       
-       <button class="button" onclick="meusLivros();">Meus Livros</button>
-       <button class="button" onclick="livrosDisponiveis();">Livros Disponiveis</button>
-       <button class="button" onclick="cadastraLivro();">Cadastrar</button>
-       <!--<button class="button" onclick="deletaLivro();">Deletar</button>-->
-       <button class="button" onclick="pesquisaLivro();">Pesquisar</button>
-       
-       
-   </div>
-    
-    <br/>
-    <br/>
-    <br/>
-    
-    <form  name="Insere Dados" action="http://localhost/SeboEletronicov2.0/Utilidades/RecebeFormLivro.php" method="post" class="formu">
-        
-                <table class='insr'>
+        <title>Sebo Eletrônico</title>
 
-                <tr>
-                    <th class='titlein' > <h5>Pesquisar Livro</h5></th>
-                </tr>
-        
-                <tr>
-                    <td > 
-                        <h4> Título: <input type="text" name="titulo" required/></h4>
-                    </td>
-                </tr>
-                
-                <tr>              
-                    <td>
-                        <h4> Estado:</h4> 
-                            <h3>
-                                <input type="checkbox" name="novo" value="novo"/>Novo
-                                <input type="checkbox" name="usado" value="usado"/>    Usado<br/>
-                            <h3/>
-                        
-                    </td>    
-                </tr>
-                
-                <tr>              
-                    <td>
-                        <h4> Classificação:</h4> 
-                            <h3>
-                                <input type="checkbox" name="venda" value="venda"/>Venda
-                                <input type="checkbox" name="troca" value="troca"/>    Troca<br/>
-                            <h3/>
-                        
-                    </td>    
-                </tr>
+    </head>
+    <body>
+        <div class="container">
+            <?php include_once '../Utilidades/BarraNavegacao.php';?>
+            <br><br><br>
+        </div>
 
-                <th>
-                    <input type="hidden" name="tipo" value="pesquisaLivro"/>
-                    <input type="submit" name='Enviar' value="Pesquisar" title='Pesquisar Livro' />
-                </th>
-
-                </table>    
-        
-    </form>
-    
-    
-</body>
-
-
+        <div class="center-block" style="width: 68.66666666666667%;">
+            <h2>Pesquisa de livros</h2>
+            <br><br><br>
+            <form class="form-horizontal" name="FrmPesquisaLivro" action="http://localhost/SeboEletronicov2.0/Utilidades/RecebeFormLivro.php" method="post" role="form">
+                <div class="form-group">
+                    <label for="titulo" class="col-sm-2 control-label">Título</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control " name="titulo" id="titulo" placeholder="Título do livro" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="estado" class="col-sm-2 control-label">Estado</label>
+                    <div class="col-sm-10">
+                        <input type="checkbox" name="novo" value="novo"/> Novo
+                        <input type="checkbox" name="usado" value="usado"/>    Usado<br/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="classificacao" class="col-sm-2 control-label">Classificação</label>
+                    <div class="col-sm-10">
+                        <input type="checkbox" name="venda" value="venda"/> Venda
+                        <input type="checkbox" name="troca" value="troca"/>    Troca<br/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <input type="hidden" name="tipo" value="pesquisaLivro"/>
+                        <input class="btn btn-primary" type="submit" name='Pesquisar' value="Pesquisar" title='Pesquisar Livro'/>
+                        <input class="btn btn-default" id="restauraDados" type="reset" name='Limpar' value="Limpar campos" />
+                        <div class="pull-right">
+                            <input class="btn btn-default" type="button" onclick="home()" style="width: 85px;" value="Cancelar" title='Cancelar'/>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
 </html>
 
 
