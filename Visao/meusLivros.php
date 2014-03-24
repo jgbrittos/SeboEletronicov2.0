0b62059983fccc16eb77aa5a79894e673c66454c
jgbs <joaogabrieldebrittoesilva@gmail.com>
2014-03-24 19:40:06 -0300

    corrigindo bug exclusao de livros

--- Visao/meusLivros.php
+++ Visao/meusLivros.php
@@ -42,7 +42,10 @@ $listaLivros = LivroControlador::getLivroByIdUsuario($id);
                 <tbody>
                     <?php
                     if ($listaLivros) {
+                        $i = 1;
                         foreach ($listaLivros as $chave => $valor) {
+                            $nomeModal = "modalExcluiLivro" . $i;
+                            $i++;
                             ?>  
                             <tr>
                                 <td><?php echo $valor['titulo_livro'] ?></td>
@@ -53,7 +56,7 @@ $listaLivros = LivroControlador::getLivroByIdUsuario($id);
                                 <td><?php
                                     echo $valor['venda'];
                                     echo "<br/>";
-                                    echo $valor['troca']
+                                    echo $valor['troca'];
                                     ?>
                                 </td>
                                 <td><?php echo $valor['genero'] ?></td>
@@ -62,46 +65,45 @@ $listaLivros = LivroControlador::getLivroByIdUsuario($id);
                                     <a href="http://localhost/SeboEletronicov2.0/Visao/alterarLivro.php?id=<?php echo $valor['id_livro'] ?> " title="Alterar Livro"> <img src="img/icone_lapis.png" align="left"> </a> <br />
                                 </td>
                                 <td>
-                                    <a data-toggle="modal" data-target="#modalExcluiLivro" href="#" title="Excluir Livro"> <img src="img/icone_lixeira.png" align="right" ></a>
+                                    <a data-toggle="modal" data-target="<?php echo "#" . $nomeModal ?>" href="#" title="Excluir Livro"> <img src="img/icone_lixeira.png" align="right" ></a>
                                 </td>
                             </tr>
-                    <?php
-                }
-            } else {
-                ?>
-            </tbody>
-        </table>
-        <div class="alert alert-info">Ops! Parece que você não possui nenhum livro cadastrado no sistema!</div>
-        <?php
-    }
-    ?>
-</div>
-
-<!-- Modal Excluir livro -->
-<div class="modal fade" id="modalExcluiLivro" tabindex="-1" role="dialog" aria-labelledby="modalExcluiLivroLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="modalNaoPublicarLabel"><b>Atenção!</b></h3>
-            </div>
-            <div class="modal-body">
-                <p style="text-align: left">
-                    <br />
-                        <h4>Ao excluir este livro não será possível restaurá-lo.</h4>
-                    <br />
-                </p>
-                <p style="text-align: left; font-size: larger"><b>Deseja confirmar a exclusão?</b></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="window.location='http://localhost/SeboEletronicov2.0/Utilidades/RecebeFormLivro.php?id_livro=<?php echo $valor['id_livro'] ?>'">Confirmar</button>
-            </div>
+                            <!-- Modal Excluir livro -->
+                        <div class="modal fade" id="<?php echo $nomeModal; ?>" tabindex="-1" role="dialog" aria-labelledby="modalExcluiLivroLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h3 class="modal-title" id="modalNaoPublicarLabel"><b>Atenção!</b></h3>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p style="text-align: left">
+                                            <br />
+                                        <h4>Ao excluir este livro não será possível restaurá-lo.</h4>
+                                        <br />
+                                        </p>
+                                        <p style="text-align: left; font-size: larger"><b>Deseja confirmar a exclusão?</b></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="window.location = 'http://localhost/SeboEletronicov2.0/Utilidades/RecebeFormLivro.php?id_livro=<?php echo $valor['id_livro'] ?>'">Confirmar</button>
+                                    </div>
+                                </div>
+                                <!-- /.modal-content -->
+                            </div>
+                            <!-- /.modal-dialog -->
+                        </div>
+                        <!-- /.modal -->
+                        <?php
+                    }
+                } else {
+                    ?>
+                    </tbody>
+                </table>
+                <div class="alert alert-info">Ops! Parece que você não possui nenhum livro cadastrado no sistema!</div>
+                <?php
+            }
+            ?>
         </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
-</body>
+    </body>
 </html>
 
