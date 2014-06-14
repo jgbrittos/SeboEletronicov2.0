d42d92165e58b6156e7f6ac5c79677e65ea7c910
caiquepereira <caiquepereira@gmail.com>
2014-06-14 11:26:36 -0300

    Alterei os caminhos no detalhes livro da Visao

--- Visao/detalheslivro.php
+++ Visao/detalheslivro.php
@@ -11,7 +11,7 @@ $id_usuario = $_SESSION['id_usuario'];
         <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
         <script type="text/javascript" src="js/js/bootstrap.3.0.3/bootstrap.js"></script>
         <script type="text/javascript" src="js/js/compressedProductionJquery.2.0.3.js"></script>
-        <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
+        <script src="../Utilidades/Redireciona.js"></script> 
         <title>Sebo Eletrônico</title>
     </head>
     <body>
@@ -60,7 +60,7 @@ $id_usuario = $_SESSION['id_usuario'];
                 <td><?php echo $livro->getGenero() ?></td>
                 <td><?php echo $livro->getEstado() ?></td>
 <!--                <td>
-                    <a href="http://localhost/SeboEletronicov2.0/Visao/alterarLivro.php?id=<?php // echo $valor['id_livro'] ?> " title="Alterar Livro"> <img src="img/icone_lapis.png" align="left"> </a> <br />
+                    <a href="../Visao/alterarLivro.php?id=<?php // echo $valor['id_livro'] ?> " title="Alterar Livro"> <img src="img/icone_lapis.png" align="left"> </a> <br />
                 </td>
                 <td>
                     <a data-toggle="modal" data-target="<?php // echo "#" . $nomeModal ?>" href="#" title="Excluir Livro"> <img src="img/icone_lixeira.png" align="right" ></a>
