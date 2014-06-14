360a9b3410ccdea1a394e1fdb6c563c2052364a6
caiquepereira <caiquepereira@gmail.com>
2014-06-14 11:23:21 -0300

    Alterei os caminhos no cadastrar livro da Visao

--- Visao/cadastrarLivro.php
+++ Visao/cadastrarLivro.php
@@ -10,7 +10,7 @@ $textoInformativo = "Ao não marcar nenhuma das opções, será considerado que 
         <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
         <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
         <link rel="stylesheet" href="Css/estilo.css"/>
-        <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
+        <script src="../Utilidades/Redireciona.js"></script> 
         <title>Sebo Eletrônico</title>
 
     </head>
@@ -21,7 +21,7 @@ $textoInformativo = "Ao não marcar nenhuma das opções, será considerado que 
             <h2>Cadastrar livro</h2>
             <br><br>
             <div class="center-block" style="width: 68.66666666666667%;">
-                <form class="form-horizontal" name="FrmPesquisaLivro" action="http://localhost/SeboEletronicov2.0/Utilidades/RecebeFormLivro.php" method="post" role="form">
+                <form class="form-horizontal" name="FrmPesquisaLivro" action="../Utilidades/RecebeFormLivro.php" method="post" role="form">
                     <div class="form-group">
                         <label for="titulo" class="col-sm-2 control-label">Título</label>
                         <div class="col-sm-10">
