00805bf27c64443312018e8dd6c45a5cd9d618f1
caiquepereira <caiquepereira@gmail.com>
2014-06-14 11:26:00 -0300

    Alterei os caminhos do compra livro na Visao

--- Visao/compralivro.php
+++ Visao/compralivro.php
@@ -13,7 +13,7 @@ $id_dono = $_POST['idDono'];
     <link rel="stylesheet" href="Css/estilo.css"/>
     <script type="text/javascript" src="js/js/compressedProductionJquery.2.0.3.js"></script>
     <script type="text/javascript" src="js/js/bootstrap.3.0.3/bootstrap.js"></script>
-    <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
+    <script src="../Utilidades/Redireciona.js"></script> 
     <title>Sebo Eletrônico</title>
 </head>
 <body>
