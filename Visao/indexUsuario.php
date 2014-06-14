a5546bbe20dedfb16498e3486b3dfca687934a84
caiquepereira <caiquepereira@gmail.com>
2014-06-14 11:47:27 -0300

    Alterei os caminhos na index Usuario da Visao

--- Visao/indexUsuario.php
+++ Visao/indexUsuario.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
         <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
         <link rel="stylesheet" href="Css/estilo.css"/>
-        <script src="http://localhost/SeboEletronicov2.0/Utilidades/Redireciona.js"></script> 
+        <script src="../Utilidades/Redireciona.js"></script> 
     <title>Sebo Eletrônico</title>
     
 </head>
@@ -15,7 +15,7 @@
     
    <div id="mainmenu">
         
-        <img src="http://localhost/SeboEletronicov2.0/Visao/img/livroUsuario.png" class="img2"/>
+        <img src="../Visao/img/livroUsuario.png" class="img2"/>
    </div>
     
     
