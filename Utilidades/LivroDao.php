d92939e5dc3708922284840f6d1ee934165262a7
caiquepereira <caiquepereira@gmail.com>
2014-06-29 18:07:59 -0300

    adicionando metodo de pesquisa

--- Utilidades/LivroDao.php
+++ Utilidades/LivroDao.php
@@ -3,4 +3,5 @@
 interface LivroDao {
     public function salvaLivro($livro, $id_dono);
     public function alteraLivro($livro, $id_dono, $id_usuario);
+    public function pesquisaLivro($titulo);
 }
