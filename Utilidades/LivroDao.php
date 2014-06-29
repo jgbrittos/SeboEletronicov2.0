cc52ca5dca501429e2d3f008fb2e1e4ad518d4c6
caiquepereira <caiquepereira@gmail.com>
2014-06-29 20:00:18 -0300

    transformando em classe abstrata para criar um metodo default de define tipo do livro

--- Utilidades/LivroDao.php
+++ Utilidades/LivroDao.php
@@ -1,7 +1,16 @@
 <?php
 
-interface LivroDao {
-    public function salvaLivro($livro, $id_dono);
-    public function alteraLivro($livro, $id_livro);
-    public function pesquisaLivro($titulo);
+include "../Utilidades/ConexaoComBanco.php";
+
+abstract class LivroDao {
+    public abstract function salvaLivro($livro, $id_dono);
+    public abstract function alteraLivro($livro, $id_livro);
+    public abstract function pesquisaLivro($titulo);
+    
+    public function defineTipoLivro($id_livro){
+        $sql = "SELECT caminhoLivroEletronico FROM livro WHERE id_livro = '".$id_livro."'";
+        $result = mysql_query($sql);
+        return mysql_fetch_array($result);
+    }
+    
 }
