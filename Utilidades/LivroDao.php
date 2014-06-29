1b8a33e33f068be2502e75de0eae6279bc61f371
caiquepereira <caiquepereira@gmail.com>
2014-06-29 16:09:20 -0300

    alterando a interface para remover metodo que nao precisa estar la

--- Utilidades/LivroDao.php
+++ Utilidades/LivroDao.php
@@ -2,7 +2,5 @@
 
 interface LivroDao {
     public function salvaLivro($livro, $id_dono);
-    public function pesquisaLivro($titulo);
     public function alteraLivro($livro, $id_dono, $id_usuario);
-    public function deletaLivro($id);
 }
