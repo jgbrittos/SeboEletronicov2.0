2c024dc2a9649ab82ad1f816fd8ec8247c93255c
caiquepereira <caiquepereira@gmail.com>
2014-06-29 18:07:43 -0300

    alterando script de conexao para facilitar o deploy no servidor

--- Utilidades/ConexaoComBanco.php
+++ Utilidades/ConexaoComBanco.php
@@ -1,15 +1,25 @@
 <?php
     
-        $server = "localhost";
-        $username = "root";
-        $senha = "";
-        $dbcon = mysql_connect($server, $username, $senha);
-        
-        if(!$dbcon){
-            die ("Não foi possível conectar: " . mysql_error());
-        }
-        $bd = mysql_select_db ("sebo eletronico");
-        
-      
-   
-?>
+    $server = "localhost";
+    $username = "root";
+    $senha = "";
+    $dbcon = mysql_connect($server, $username, $senha);
+
+    if(!$dbcon){
+        die ("Não foi possível conectar: " . mysql_error());
+    }
+    $bd = mysql_select_db ("sebo eletronico");       
+
+    /*
+
+    $server = "mysql.hostinger.com.br";
+    $username = "u367438131_root";
+    $senha = "seboeletronicodsw";
+    $dbcon = mysql_connect($server, $username, $senha);
+
+    if(!$dbcon){
+        die ("Não foi possível conectar: " . mysql_error());
+    }
+    $bd = mysql_select_db ("u367438131_sebo");
+
+    */
