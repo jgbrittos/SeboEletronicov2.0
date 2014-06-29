54d476d8e83a907ab44639e87e0ef2b4006f1103
caiquepereira <caiquepereira@gmail.com>
2014-06-29 18:30:17 -0300

    alterando a interface para mudar o metodo de alterar livro

--- Utilidades/LivroDao.php
+++ Utilidades/LivroDao.php
@@ -2,6 +2,6 @@
 
 interface LivroDao {
     public function salvaLivro($livro, $id_dono);
-    public function alteraLivro($livro, $id_dono, $id_usuario);
+    public function alteraLivro($livro, $id_livro);
     public function pesquisaLivro($titulo);
 }
