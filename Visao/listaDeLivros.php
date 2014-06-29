b118b70f9e4565ccfeeb5a1fefd25d0cd611e55d
caiquepereira <caiquepereira@gmail.com>
2014-06-29 16:09:53 -0300

    alterando a view de lista de livros para comportar os dois tipos de livros

--- Visao/listaDeLivros.php
+++ Visao/listaDeLivros.php
@@ -10,15 +10,14 @@ $listaLivros = $livroControlador->pesquisaLivro($titulo);
 
 <html>
 <head>	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
-        <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
-        <link rel="stylesheet" href="Css/estilo.css"/>
-        <script type="text/javascript" src="js/js/compressedProductionJquery.2.0.3.js"></script>
-        <script type="text/javascript" src="js/js/bootstrap.3.0.3/bootstrap.js"></script>
-        <script src="../Utilidades/Redireciona.js"></script> 
-        <title>Sebo Eletrônico</title>
-    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="Css/bootstrap.3.0.3/bootstrap.css"/>
+    <link rel="stylesheet" href="Css/todc-bootstrap.3/todcBootstrap.3.css"/>
+    <link rel="stylesheet" href="Css/estilo.css"/>
+    <script type="text/javascript" src="js/js/compressedProductionJquery.2.0.3.js"></script>
+    <script type="text/javascript" src="js/js/bootstrap.3.0.3/bootstrap.js"></script>
+    <script src="../Utilidades/Redireciona.js"></script> 
+    <title>Sebo Eletrônico</title>
 </head>
 <body>
     <div class="container">
